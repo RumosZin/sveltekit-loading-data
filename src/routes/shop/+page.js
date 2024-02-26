b7f2afce1c12ae9089977a402cf4cfc8b0995359
yunjin Kim <81238093+RumosZin@users.noreply.github.com>
2024-02-26 17:50:44 +0900

    products, users 두 요청을 병렬로 실행하는 방법
    - 각 요청에 대해 fetch{} 함수를 만들고 반환

--- src/routes/shop/+page.js
+++ src/routes/shop/+page.js
@@ -1,15 +1,22 @@
 
 export const load = async ({fetch}) => {
 
-    const productRes = await fetch('https://dummyjson.com/products?limit=10')
-    const productData = await productRes.json()
-    const products = productData.products
+    const fetchProducts = async() => {
+        const productRes = await fetch('https://dummyjson.com/products?limit=10')
+        const productData = await productRes.json()
+        return productData.products
+    }
 
-    const usersRes = await fetch('https://dummyjson.com/users?limit=10')
-    const usersData = await usersRes.json()
-    const users = usersData.products
+    const fetchUsers = async () => {
+        const usersRes = await fetch('https://dummyjson.com/users?limit=10')
+        const usersData = await usersRes.json()
+        return usersData.products
+    }
+    
+
+    
     return {
-        products : products,
-        users: users
+        products : fetchProducts(),
+        users: fetchUsers()
     }
 }
